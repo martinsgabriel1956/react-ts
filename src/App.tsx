325bca8341868f5f78a044336fd3d782d187111d
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-15 17:19:19 -0300

    feat: managing state and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 import { Todos } from "./components/Todos";
 import Todo from "./models/todo";
 import { NewTodo } from "./components/Todos/NewTodo";
 
 export function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAddTodo = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    })
   }
 
   return (
